593736641b1caec41b684120238f5c28cdaaad78
Carmem Vanessa <vanessacarmem0713@gmail.com>
2026-05-26 15:44:39 -0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,10 +2,23 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture
-def driver():
+def driver(request):
 
     driver = webdriver.Edge()
 
     yield driver
 
+    if request.node.rep_call.failed:
+
+        driver.save_screenshot(f"screenshots/{request.node.name}.png")
+
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
