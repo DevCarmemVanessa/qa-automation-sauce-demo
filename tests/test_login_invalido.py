8b73b9cdb9ca9390c7c19af9bb522cd549bc8633
Carmem Vanessa <vanessacarmem0713@gmail.com>
2026-05-26 15:20:20 -0300

    Update test_login_invalido.py

--- tests/test_login_invalido.py
+++ tests/test_login_invalido.py
@@ -1,30 +1,17 @@
 from selenium import webdriver
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
-import time
 
 def test_login_invalido():
 
     driver = webdriver.Edge()
 
-    driver.get("https://www.saucedemo.com/")
+    login_page = LoginPage(driver)
 
-    time.sleep(5)
-
-    campo_usuario = driver.find_element(By.ID, "user-name")
-    campo_senha = driver.find_element(By.ID, "password")
-    botao_login = driver.find_element(By.ID, "login-button")
-
-    campo_usuario.send_keys("usuario_errado")
-
-    time.sleep(5)
-
-    campo_senha.send_keys("senha_errada")
-
-    time.sleep(5)
-
-    botao_login.click()
-
-    time.sleep(5)
+    login_page.acessar_site()
+    login_page.preencher_usuario("usuario_errado")
+    login_page.preencher_senha("senha_errada")
+    login_page.clicar_login()
 
     mensagem_erro = driver.find_element(By.TAG_NAME, "h3")
 
