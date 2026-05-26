c60ae23341974808b7324516face46f1784aa693
Carmem Vanessa <vanessacarmem0713@gmail.com>
2026-05-26 14:48:56 -0300

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 def test_login_valido():
 
@@ -7,12 +8,20 @@ def test_login_valido():
 
     driver.get("https://www.saucedemo.com/")
 
+    time.sleep(2)
+
     campo_usuario = driver.find_element(By.ID, "user-name")
     campo_senha = driver.find_element(By.ID, "password")
     botao_login = driver.find_element(By.ID, "login-button")
 
     campo_usuario.send_keys("standard_user")
+    
+    time.sleep(2)
+    
     campo_senha.send_keys("secret_sauce")
+
+    time.sleep(2)
+    
     botao_login.click()
 
     assert "inventory" in driver.current_url
