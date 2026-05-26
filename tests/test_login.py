1a7c0bcca67afcf203fe38d8e9680f00736e1bbf
Carmem Vanessa <vanessacarmem0713@gmail.com>
2026-05-26 14:45:03 -0300

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-def test_login():
+def test_login_valido():
 
     driver = webdriver.Edge()
 
     driver.get("https://www.saucedemo.com/")
 
-    assert "saucedemo" in driver.current_url
+    campo_usuario = driver.find_element(By.ID, "user-name")
+    campo_senha = driver.find_element(By.ID, "password")
+    botao_login = driver.find_element(By.ID, "login-button")
+
+    campo_usuario.send_keys("standard_user")
+    campo_senha.send_keys("secret_sauce")
+    botao_login.click()
+
+    assert "inventory" in driver.current_url
 
     driver.quit()
